f804bbc97e10a54b2371b8dae264f5fa0f8cbbe1
faizan9262 <faizanshaikh9262@gmail.com>
2025-06-05 20:41:05 +0530

    intizalized for vercel

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
-import app from "./app.js";
-import { connectDB } from "./src/config/databaseConnection.js";
+import express from "express";
+import { config } from "dotenv";
+import appRouter from "./src/routes/index.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectCloudinary from "./src/config/cloudinary.js";
 
-const port = process.env.PORT;
-connectDB().then(() => {
-  app.listen(port, () => {
-    console.log(`App running on ${port} & Databse Connected`);
-  });
-}).catch((err)=>{
-    console.log(err);
-})
+const app = express();
+
+config();
+connectCloudinary();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+app.use("/api", appRouter);
+
+export default app;
