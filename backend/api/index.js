20385bb69ba6ee129a34b75447cb4a600b6bdd2b
faizan9262 <faizanshaikh9262@gmail.com>
2025-06-05 22:47:02 +0530

    vercel node server added

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,8 @@
 import app from "../index.js";
-import { createServer } from "http";
+import { createServer } from "vercel-node-server";
 
-export default async function handler(req, res) {
-  const server = createServer(app);
+const server = createServer(app);
+
+export default function handler(req, res) {
   server.emit("request", req, res);
 }
