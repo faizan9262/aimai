3672dfc5a47bd314921afe52112e2411df944cc8
faizan9262 <faizanshaikh9262@gmail.com>
2025-06-05 22:10:45 +0530

    backend cors modified

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,13 @@ app.use(
   cors({
     origin: "https://aimai.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use("/api", appRouter);
