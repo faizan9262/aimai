2e77f5ec6059550cf623bac597d32922078736a5
faizan9262 <faizanshaikh9262@gmail.com>
2025-06-06 19:35:39 +0530

    database connection updated

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config()
 
 const app = express();
 
-connectDB();
+connectDB().then(() => console.log("DB connected")).catch(console.error)
 connectCloudinary();
 
 const allowedOrigins = ["https://aimai-frontend.vercel.app"];
